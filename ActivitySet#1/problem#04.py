313e1627def6ace66e40b2762351f86397fd5563
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-05-14 11:49:41 +0000

    problem#04.py(1 and 2)

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,4 +1,4 @@
-# Conditional Execution
+# Conditional Execution(1)
 
 hrs = input("Enter Hours:")
 h = float(hrs)
@@ -8,4 +8,24 @@ if h>40:
     pay=print((h-40)*1.5*r+40*r)
 else:
     pay=print(h*r)
-    
+
+
+# Conditional Execution(2)
+
+score = input("Enter Score: ")
+i=float(score)
+if i>0.0 and i<1.0:
+    if i>=0.9:
+       print('A')
+    elif i>=0.8:
+       print('B')
+    elif i>=0.7:
+       print('C')
+    elif i>=0.6:
+       print('D')
+    elif i<0.6:
+       print('F')
+        
+else:
+     print('Error:The value is out of the range!!!!!')
+        
