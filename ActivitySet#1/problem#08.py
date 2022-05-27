e755c5db3d24cb22440e2f7a83ebdeb886ec9d34
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-05-27 04:39:12 +0000

    problem#08.py

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,16 @@
 # Files
 
 filename = "dataset/mbox-short.txt"
+fhand=input("Enter the file")
+fh=open(fhand)
+count=0
+total=0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    num=float(line[line.find("0"):])  
+    count=count+1
+    total=total+num
+    average=total/count
+    
+print("Average spam confidence:",average)
