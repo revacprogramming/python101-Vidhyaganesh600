39b81ed5461ffa6c6c2a0a8d3763e039aa7df3a1
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-05-30 16:16:28 +0000

    problem#08.py (practice)

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -14,3 +14,18 @@ for line in fh:
     average=total/count
     
 print("Average spam confidence:",average)
+
+"""fname = input("Enter file name: ")
+fh = open(fname)
+count=0
+total=0
+for line in fh:
+    if  not line.startswith("X-DSPAM-Confidence:"):
+     continue
+    #print(line)
+    num=float(line[line.find("0"):])
+    count=count+1
+    total=total+num
+   
+print("Average spam confidence:",total/count)"""
+ 
