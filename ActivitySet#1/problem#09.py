600c74e25e7b087ab5c6fead38f4c3ed03734286
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-05-31 09:25:43 +0000

    problem#09.py

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,13 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+#filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fn = open(fname)
+lst = []
+for l in fn:
+    #l = l.rstrip()
+    l = l.split()
+    for word in l:
+    	if word not in lst:
+        	lst.append(word)
+print(sorted(lst))
