e1a1d710ecc843c109a810c7eaedd98a5261a17f
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-06-02 16:22:32 +0000

    problem#07.py(practice)

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,5 +1,11 @@
 # Strings
 
+'''text = "X-DSPAM-Confidence:    0.8475"
+#length=len(text)
+#print(text[length-6:length])
+index=text.find('0.8475')
+print(float(text[index: ]))'''
+
 text = "X-DSPAM-Confidence:    0.8475"
 #length=len(text)
 #print(text[length-6:length])
