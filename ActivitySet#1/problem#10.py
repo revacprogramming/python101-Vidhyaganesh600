75656b726d94f39de017fdff62c1ba50821fb91c
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-06-07 09:22:36 +0000

    problem#10.py

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,23 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+
+d=dict()
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+for i in handle:
+    if i.startswith("From "):
+        emails=i.split()
+        email=emails[1]
+        d[email]=d.get(email,0)+1
+    else:
+        continue
+print(email,d[email])
+
+
+
+
+
+
