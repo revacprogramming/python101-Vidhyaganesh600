200ff01aa8ac2b5e7c6c3ca3c988c8644e144163
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-06-10 04:50:47 +0000

    PROBLEM#11.PY

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,7 +1,7 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
-name = input("Enter file:")
+'''name = input("Enter file:")
 if len(name) < 1:
     name = "mbox-short.txt"
 handle = open(name)
@@ -12,10 +12,33 @@ for line in handle:
         #x=tuple(lin[0])
         x=lin[0]
         hour = x[0:2]
-        '''for x in hour:
+        for x in hour:
              if x in hour:
                   
              else:
-                  count=1'''
+                  count=1
         h=hour.sort()
-        print(h)
+        print(h)'''
+l=list()
+d=dict()
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+for info in handle:
+    if info.startswith("From "):
+        inf=info.split()
+        part=inf[5]
+        par=part[ :2]
+        l.append(par)
+        l.sort()
+for x in l:
+       if x not in d:
+              d[x] =1
+       else:
+              d[x]=d[x] + 1
+
+for k,v in d.items():
+   print(k,v)
+   
+        
