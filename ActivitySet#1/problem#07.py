3f26a87b7eed80a7a1d7801352541a99b9544fbd
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-06-13 16:54:59 +0000

    PROBLEM#07.PY(PRACTICE)

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -6,8 +6,14 @@
 index=text.find('0.8475')
 print(float(text[index: ]))'''
 
-text = "X-DSPAM-Confidence:    0.8475"
+'''text = "X-DSPAM-Confidence:    0.8475"
 #length=len(text)
 #print(text[length-6:length])
 index=text.find('0.8475')
+print(float(text[index: ]))'''
+
+
+text = "X-DSPAM-Confidence:    0.8475"
+index=text.find('0.8475')
 print(float(text[index: ]))
+
