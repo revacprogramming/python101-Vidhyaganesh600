61300f9561cdf5b7884c6f95a55fde7ae8aa66da
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-07-01 09:57:33 +0530

    Update problem#12.py

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,15 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+import re
+sum=0
+file=input(" \033[1m Enter the filename -> \033[0m")
+hand=open(file)
+for line in hand:
+           line=line.rstrip()
+           x = re.findall('\d+',line)
+           for k in x:
+              y=float(k)
+              sum+=y
+print(sum)
+
+
