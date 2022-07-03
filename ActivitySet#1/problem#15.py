8360b79fc18e2dc4187cb4a02cdbf4bbb6cb26bc
Vidhyaganesh600 <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-07-03 16:23:47 +0000

    problem#17.py

--- ActivitySet#1/problem#15.py
+++ ActivitySet#1/problem#15.py
@@ -1,2 +1,10 @@
 # Object Oriented Programming
 # https://www.py4e.com/lessons/Objects
+cart=list()
+cart.append("Soaps")
+cart.append("Bottles")
+cart.append("Box")
+cart.sort()
+print(cart[0])
+print(cart.__getitem__(1))
+print(list.__getitem__(cart,2))
