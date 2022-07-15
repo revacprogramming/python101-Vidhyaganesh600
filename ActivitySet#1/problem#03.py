06d8ffd5191208a93075af30d29970b67ed9b941
VIDHYA GANESH.A <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-07-15 17:28:37 +0000

    Act#3prob#1.py

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -10,10 +10,4 @@ rat = float(input("Enter rate:"))
 Pay = hrs*rat
 print("Pay:", float(Pay))
 
-hrs = float(input("Enter Hours:"))
-rat = float(input("Enter rate:"))
-Pay = hrs*rat
-print("Pay:", float(Pay)
 
-#DONE
-#P
---
+++ ActivitySet#3/problem#1.py
@@ -0,0 +1,24 @@
+import math
+"""n=int(input("Enter the number of rectangles"))
+i=1
+if i<=n"""
+
+class point:
+    def __init__(self,x,y)
+          self.x=x
+          self.y=y
+
+    def distance(self,l,m,j):
+        l=math.sqrt((x2-x1)**2 - (y2-y1)**2)
+        m=math.sqrt((x3-x2)**2 - (y3-y2)**2)
+        j=l*m
+        return j
+
+
+
+p=point(x,y)
+point__init__((x1,y1),(0.0,0.0))
+p2=point((x2,y2),(0.0,1.0))
+p3=point((x3,y3),(1.0,0.0))        
+
+
