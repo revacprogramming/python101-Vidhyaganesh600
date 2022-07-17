239962c3dd15cd593ff172b6149a58e4268b377d
VIDHYA GANESH.A <97090593+Vidhyaganesh600@users.noreply.github.com>
2022-07-17 16:13:28 +0000

    set#3prob#2(working)

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -10,3 +10,20 @@ m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special method
 print(m)  # should print the menu properly
 
 m=input("enter the items")
+
+
+class Menu():
+  """fill in class definition here"""
+  def __init__(self,item,rate):
+    self.item=item
+    self.rate=rate
+  def __add__(self,other):
+    return self.item + str(self.rate),other.item+str(other.rate)
+
+
+m1=Menu('idly',10)
+m2=Menu('vada', 20)
+m3=Menu('dosa',30)
+# Hint: operator overloading special methods (__add__, __sub__, etc.)
+print("__________Menu___________")  # should print the menu properly
+print(m1 +m2)
